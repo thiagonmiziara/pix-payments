4845350cf9584564ec29dc23eccef37f9cc6d940
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-24 14:52:00 -0300

    feat: Adding Dynamic Data to the Payments Page

--- payments/pix.py
+++ payments/pix.py
@@ -12,9 +12,9 @@ class Pix:
         hash_payment = f"bank_payment_id_{bank_payment_id}"
 
         img = qrcode.make(hash_payment)
-        img.save(f"static/img/qr_code_payment_{hash_payment}.png")
+        img.save(f"static/img/qr_code_{bank_payment_id}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code_path": f"qr_code_payment_{bank_payment_id}.png",
+            "qr_code_path": f"qr_code_{bank_payment_id}",
         }
