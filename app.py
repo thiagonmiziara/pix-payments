3a0f2f542a8b950aee49100134668dcefaecd10d
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-24 11:25:47 -0300

    feat: Storing QR Codes and creating image display routes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request, send_file
 from repository.database import db
 from db_models.payment import Payment
-from datetime import datetime, timedelta
+from payments.pix import Pix
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -18,6 +19,11 @@ def create_payment_pix():
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
+
     db.session.add(new_payment)
     db.session.commit()
 
@@ -30,6 +36,11 @@ def create_payment_pix():
         ),
         201,
     )
+
+
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
+def get_image(file_name):
+    return send_file(f"static/img/{file_name}.png", mimetype="image/png")
 
 
 # webhook
--- payments/pix.py
+++ payments/pix.py
@@ -7,14 +7,14 @@ class Pix:
         pass
 
     def create_payment(self):
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f"bank_payment_id_{bank_payment_id}"
 
         img = qrcode.make(hash_payment)
-        img.save(f"static/imgs/qr_code_payment_{hash_payment}.png")
+        img.save(f"static/img/qr_code_payment_{hash_payment}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code_path": f"static/imgs/qr_code_payment_{hash_payment}.png",
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}.png",
         }
