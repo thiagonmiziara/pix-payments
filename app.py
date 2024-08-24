0d1ad61d532c1973c5857511461d47f43611e19a
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-24 16:35:25 -0300

    feat: 404 Page and Client Disconnection from Server

--- app.py
+++ app.py
@@ -74,6 +74,9 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if not payment:
+        return render_template("404.html")
+
     if payment.paid:
         return render_template(
             "confirmed_payment.html",
@@ -94,6 +97,11 @@ def payment_pix_page(payment_id):
 @socketio.on("connect")
 def handle_connect():
     print("Client connected to the server")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected from the server")
 
 
 if __name__ == "__main__":
