d035a162c175862b079d6c4f442849513ece3e7d
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-24 15:10:48 -0300

    feat: Implementing Websockets in the Application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from flask import Flask, jsonify, request, send_file, render_template
+from flask_socketio import SocketIO
 from repository.database import db
 from db_models.payment import Payment
 from payments.pix import Pix
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
 db.init_app(app)
+socketio = SocketIO(app)
 
 
 @app.route("/payments/pix", methods=["POST"])
@@ -62,5 +64,10 @@ def payment_pix_page(payment_id):
     )
 
 
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected to the server")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
